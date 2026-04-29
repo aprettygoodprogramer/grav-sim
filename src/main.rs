e127e5af1c5fe483239c3487d50351569900c971
Evan Ducas <evan.j.ducas@gmail.com>
2026-04-28 20:18:55 -0400

    Added body struct, and basic rendering.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,38 @@
-use macroquad::prelude::*;
+use macroquad::{color, prelude::*};
+mod structs;
+use structs::body;
 
-#[macroquad::main("MyGame")]
+
+#[macroquad::main("Grav Sim")]
 async fn main() {
+    let mut bodies: Vec<body> = Vec::new();
+    let mut example_body = body{
+mass: 100f32,
+vel:  vec2(100f32, 100f32),
+pos: vec2(10f32, 10f32),
+color: RED
+    };
+    bodies.push(example_body);
+
     loop {
-        clear_background(RED);
+        clear_background(BLACK);
+        for body in &bodies {
+            let radius = body.mass.sqrt().clamp(1.0, 10.0);
+            draw_circle(body.pos.x, body.pos.y, radius, body.color);
+        }
 
-        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
-        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
-
-        draw_text("Hello, Macroquad!", 20.0, 20.0, 30.0, DARKGRAY);
 
         next_frame().await
     }
 }
      
+
+
+
+
+
+// Planning:
+
+// I need a body object, with mass, and position
+
+// Then every frame i need to calculate and move each object using gravity formula
---
+++ src/structs.rs
@@ -0,0 +1,9 @@
+use macroquad::prelude::*;
+
+pub struct body {
+pub mass: f32,
+pub vel: Vec2,
+pub pos: Vec2,
+pub color: Color
+
+}
